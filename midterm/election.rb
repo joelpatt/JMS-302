816c6d5329898eb3363c51c052821886038c9afc
joel.patt15@gmail.com <Joel Patterson>
2014-10-09 10:10:29 -0400

    midterm 2

---
+++ midterm/election.rb
@@ -0,0 +1,11 @@
+puts "Please enter the last name of your chosen candidate. Type 'done' when you made your selection."
+while true 
+    get_results= gets.chomp
+    if (result.downcase == 'done')
+        exit
+    else results.push
+    end
+end
+
+
+puts "{Candidate} is winning with {vote_percent}% of the votes."
