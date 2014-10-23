9d568e78810adc7cd609c6a5d1a69a7cf8742d99
joel.patt15@gmail.com <Joel Patterson>
2014-10-22 23:32:07 -0400

    week_10exercises

---
+++ homework/week_10/restaurant_generator.rb
@@ -0,0 +1,26 @@
+file = "restaurants.txt"
+unless File.exists? file
+    puts "#{file} doesn't exist"
+    exit
+end
+
+restaurants = array.new # []
+File.open(file, "r"). each_line do |line|
+    words = line.chomp
+puts " Would you like to add to the list? "
+File.update(file, "a"). = "restaurant.txt"
+
+puts " Type restaurant names. When complete type done."
+    while true
+        restaurants = gets.chomp
+        if restaurants == "done"
+        break
+        restaurant.push restaurant
+end
+            
+rand (restaurant.size)
+random_restaurant = rand (restaurants.size)
+puts " Go to restaurant #{restuarant}"
+    restaurant [random_restaurant_index]
+
+
